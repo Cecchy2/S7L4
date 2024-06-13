02a6a36db38f97bad5a08522760d59e94af92815
Cecchy2 <dariocecchinato@gmail.com>
2024-06-13 16:29:45 +0200

    collegata funzione che genera le carte al click dei bottoni

---
+++ pexel.js
@@ -0,0 +1,51 @@
+const apiKey = "pdhvStg9dyzMqX1NGZqk2n60QAhsv9kUVlYZ4zcctXONVlCfVmTbK2cU";
+/* fetch(`https://api.pexels.com/v1/search?query=${query}`); */
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loadImg = document.getElementById("load-images");
+  const loadSecImg = document.getElementById("load-secondary");
+
+  loadImg.onclick = () => {
+    loadImages("wave");
+  };
+  loadSecImg.onclick = () => {
+    loadImages("mountains");
+  };
+
+  const loadImages = (query) => {
+    fetch(`https://api.pexels.com/v1/search?query=${query}`, {
+      headers: {
+        Authorization: apiKey,
+      },
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        }
+      })
+      .then((picture) => {
+        creaImmagini(picture.photos);
+      })
+      .catch((error) => console.log("Error:", error));
+  };
+  const creaImmagini = (photos) => {
+    const gallery = document.getElementById("gallery");
+    gallery.innerHTML = "";
+
+    photos.forEach((photo) => {
+      const card = document.createElement("div");
+      card.className = "col-4 card mb-4 me-3shadow-sm";
+
+      card.innerHTML = `
+      <img src="${photo.src.medium}" alt="${photo.photographer}" class="card-img-top">
+      <div class="card-body">
+        <h5 class="card-title">${photo.photographer}</h5>
+        <p class="card-text">${photo.id}</p>
+        <a href="#" class="btn btn-primary view-image" data-id="${photo.id}" data-src="${photo.src.large}">View</a>
+        <a href="#" class="btn btn-secondary hide-card">Hide</a>
+      </div>
+    `;
+      gallery.appendChild(card);
+    });
+  };
+});
